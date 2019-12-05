852172dff9af4583380eb79af302046d587da765
Kevin Burke <kevin.burke@kevinburke-ltm6.internal.salesforce.com>
2019-12-05 15:55:28 +0000

    field tidy

--- utils/scripts/install.sh
+++ utils/scripts/install.sh
@@ -4,7 +4,7 @@ source "$mydir"/user.properties
 # config load
 
 
-sfdx force:package:install --package "04tB0000000ORWpIAO" -k $COREINSTALLATIONPASSWORD --wait 5 --publishwait 5 -s AdminsOnly
+sfdx force:package:install --package "04t1n000002aDMvAAM" -k $COREINSTALLATIONPASSWORD --wait 5 --publishwait 5 -s AdminsOnly
 
 
 # RESULT_JSON=`sfdx force:data:soql:query -t -q "SELECT id FROM SubscriberPackageVersion WHERE Name='$PACKAGENAME'"`
